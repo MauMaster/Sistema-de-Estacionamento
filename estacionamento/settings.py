62ca1af5536653dbf3c51ec2f3a31b81d3a550d1
Mauricio  Kalfelz <mauvirtual@gmail.com>
2018-11-11 13:34:35 -0200

    criando app website2

--- estacionamento/settings.py
+++ estacionamento/settings.py
@@ -88,13 +88,14 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'core_home'
